53fa2c08d8ecafb15b735282c2c65f5311b88fd0
Manh Cuong <cuongpigerr@gmail.com>
2021-09-29 14:25:18 +0700

    .

--- Python/misc/utils.py
+++
@@ -1,9 +0,0 @@
-class CUtils:
-    def seedir(self, path: str):
-        """
-            Vẽ cây thư mục
-        Args:
-            path (str): đường dẫn hiện tại của thư mục cần vẽ
-        """
-        import seedir as sd
-        sd.seedir(path, style='emoji')
